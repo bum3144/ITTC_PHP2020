09a93b92c25779869ede56309acb11ad4a502cee
bum3144 <bum3144@gmail.com>
2020-01-22 09:28:14 +0800

    php basic 01

---
+++ example01.php
@@ -0,0 +1,23 @@
+<?php
+    echo "Hello PHP! <br> ";
+    
+    // echo "Hello World!!";
+    /*
+        made by : Jeremy Park
+        date : 22 Jan 2020
+        laguage : PHP
+        location : Philippines
+    */
+    
+    // variable
+    // operator + - * / %
+    $a = 10;
+    $b = 3;
+    $c = 'School';
+
+    echo $a + $b ."<br>";
+    echo $c . " start time is " . $a . "am!<br>";
+    
+    
+    echo $a % $b;
+?>
