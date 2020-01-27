7f06a0d89364a66e3fb95f190459e50a0a469e35
bum3144 <bum3144@gmail.com>
2020-01-27 08:46:02 +0800

    example_site_include

---
+++ example_site.php
@@ -0,0 +1,139 @@
+<?php
+include 'example_site_head.php';
+?>
+	<div id="content">
+		<section id="main_section">
+			<article class="main_article">
+				<h1>Main Article</h1>
+				<p>While the Sandiganbayan acquitted former Senator Bong Revilla, it never said he is innocent, a former Supreme Court (SC) spokesperson said Tuesday.
+
+				Speaking to CNN Philippines' The Source, former SC spokesperson Theodore Te said the anti-graft court only said there was not enough evidence to prove guilt beyond reasonable doubt.
+
+				Te explained that the Rules of Court provides for two forms of acquittal – one where the court finds that there is absolutely no proof beyond reasonable doubt and another where the court says the prosecution failed to prove the case beyond reasonable doubt.</p>
+			</article>
+			<article class="main_article">
+			<h1>Main Article</h1>
+				<p>While the Sandiganbayan acquitted former Senator Bong Revilla, it never said he is innocent, a former Supreme Court (SC) spokesperson said Tuesday.
+
+				Speaking to CNN Philippines' The Source, former SC spokesperson Theodore Te said the anti-graft court only said there was not enough evidence to prove guilt beyond reasonable doubt.
+
+				Te explained that the Rules of Court provides for two forms of acquittal – one where the court finds that there is absolutely no proof beyond reasonable doubt and another where the court says the prosecution failed to prove the case beyond reasonable doubt.</p>
+			</article>
+			<article class="main_article">
+				<h1>Main Article</h1>
+				<p>While the Sandiganbayan acquitted former Senator Bong Revilla, it never said he is innocent, a former Supreme Court (SC) spokesperson said Tuesday.
+
+				Speaking to CNN Philippines' The Source, former SC spokesperson Theodore Te said the anti-graft court only said there was not enough evidence to prove guilt beyond reasonable doubt.
+
+				Te explained that the Rules of Court provides for two forms of acquittal – one where the court finds that there is absolutely no proof beyond reasonable doubt and another where the court says the prosecution failed to prove the case beyond reasonable doubt.</p>
+			</article>
+		</section>
+		<aside id="main_aside">
+			<input id="first" type="radio" name="tab" checked="checked" />
+			<input id="second" type="radio" name="tab" />
+			<section class="buttons">
+				<label for="first">First</label>
+				<label for="second">Second</label>
+			</section>
+			<div class="tab_item">
+				<ul>
+					<li class="item">
+						<a href="#">
+							<div class="thumbnail">
+								<img src="images/ProgramIcon/css3.png" />
+							</div>
+							<div class="description">
+								<strong>HTML5 Canvas</strong>
+								<p>2018-DEC-13</p>
+							</div>
+						</a>
+					</li>
+					<li class="item">
+						<a href="#">
+							<div class="thumbnail">
+								<img src="images/ProgramIcon/css3.png" />
+							</div>
+							<div class="description">
+								<strong>HTML5 Audio</strong>
+								<p>2018-DEC-13</p>
+							</div>
+						</a>
+					</li>
+					<li class="item">
+						<a href="#">
+							<div class="thumbnail">
+								<img src="images/ProgramIcon/css3.png" />
+							</div>
+							<div class="description">
+								<strong>HTML5 Video</strong>
+								<p>2018-DEC-13</p>
+							</div>
+						</a>
+					</li>
+					<li class="item">
+						<a href="#">
+							<div class="thumbnail">
+								<img src="images/ProgramIcon/css3.png" />
+							</div>
+							<div class="description">
+								<strong>HTML5 Semantic web</strong>
+								<p>2018-DEC-13</p>
+							</div>
+						</a>
+					</li>
+				</ul>
+			</div>
+			<div	class="tab_item">
+				<ul>
+					<li class="item">
+						<a href="#">
+							<div class="thumbnail">
+								<img src="images/ProgramIcon/css3.png" />
+							</div>
+							<div class="description">
+								<strong>CSS3 Transition</strong>
+								<p>2018-DEC-13</p>
+							</div>
+						</a>
+					</li>
+					<li class="item">
+						<a href="#">
+							<div class="thumbnail">
+								<img src="images/ProgramIcon/css3.png" />
+							</div>
+							<div class="description">
+								<strong>CSS3 Animation</strong>
+								<p>2018-DEC-13</p>
+							</div>
+						</a>
+					</li>
+					<li class="item">
+						<a href="#">
+							<div class="thumbnail">
+								<img src="images/ProgramIcon/css3.png" />
+							</div>
+							<div class="description">
+								<strong>CSS3 Border</strong>
+								<p>2018-DEC-13</p>
+							</div>
+						</a>
+					</li>
+					<li class="item">
+						<a href="#">
+							<div class="thumbnail">
+								<img src="images/ProgramIcon/css3.png" />
+							</div>
+							<div class="description">
+								<strong>CSS3 Box</strong>
+								<p>2018-DEC-13</p>
+							</div>
+						</a>
+					</li>
+				</ul>
+			</div>
+		</aside>
+	</div>
+
+<?php
+include "example_site_foot.php";
+?>
---
+++ example_site_foot.php
@@ -0,0 +1,12 @@
+
+	<footer id="main_footer">
+		<h3>HTML5 + CSS3 Basic</h3>
+		<address>Website Layout Basic</address>
+	</footer>
+
+	<div id="loginLayer">
+		<h1>LOG IN</h1>
+
+	</div>
+</body>
+</html>
