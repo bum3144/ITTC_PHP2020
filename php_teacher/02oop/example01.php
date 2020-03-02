de0191d1d0098bf497c08f647bc3e34d36542a66
bum3144 <bum3144@gmail.com>
2020-03-02 23:50:14 +0800

    Object Oriented Programming

---
+++ php_teacher/02oop/example01.php
@@ -0,0 +1,19 @@
+<?php
+    // error_reporting(E_ALL);
+    // ini_set('display_errors', '1');
+
+# OOP => Object-oriented programming
+
+//$myObject = new SomeClass(); // create object 객체생성
+//$myObject->someProperty = 123; // property assignment 속성할당
+//echo $myObject->someProperty; // use property 속성사용
+
+try{
+    $pdo = new PDO('mysql:host=localhost;dbname=ijdb', 'ijdbuser', 'mypassword');
+    $output = 'Connected successfully!';
+}catch (PDOExeption $e){
+    $output = 'Connection failed!' . $e;
+}
+
+echo $output;
+?>
