a37aace841a58769ca5f14ec8e4b32c69f19fda2
bum3144 <bum3144@gmail.com>
2020-03-05 09:11:45 +0800

    PDO connetion 02

--- php_teacher/02oop/example02.php
+++ php_teacher/02oop/example02.php
@@ -1,12 +1,26 @@
 <?php
+//ex 01 simple error check
 $dsn = "mysql:host=localhost;dbname=ijdb";
 try {
     $pdo = new PDO($dsn, "ijdbuser1", "mypassword");
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     $output = 'Connected successfully!!';
 } catch(PDOException $e) {
-    //echo $e->getMessage();
     $output = 'Connection failed!!' . $e;
 }
 include __DIR__ . '/output.html.php';
+
+
+// //ex 02 big size site error check
+// $dsn = "mysql:host=localhost;dbname=ijdb";
+// try {
+//     $pdo = new PDO($dsn, "ijdbuser1", "mypassword");
+//     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+//     $output = 'Connected successfully!!';
+// } catch(PDOException $e) {
+//     $output = 'Connection failed!!' . 
+//     $e->getMessage() . ', location: ' .
+//     $e->getFile() . ':' . $e->getLine();
+// }
+// include __DIR__ . '/output.html.php';
 ?>
