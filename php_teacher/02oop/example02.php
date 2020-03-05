a0d9e1621d7e3c6e6198f323fa2b3b6661332f5b
bum3144 <bum3144@gmail.com>
2020-03-05 09:00:38 +0800

    PDO connetion 02

--- php_teacher/02oop/example02.php
+++ php_teacher/02oop/example02.php
@@ -1,12 +1,12 @@
 <?php
-try{
-    $pdo = new PDO('mysql:host=localhost;dbname=ijdb;charset=utf8', 'ijdbuser1', 'mypassword');
+$dsn = "mysql:host=localhost;dbname=ijdb";
+try {
+    $pdo = new PDO($dsn, "ijdbuser1", "mypassword");
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     $output = 'Connected successfully!!';
-}catch (PDOExeption $e){
+} catch(PDOException $e) {
+    //echo $e->getMessage();
     $output = 'Connection failed!!' . $e;
 }
-
-//echo $output;
 include __DIR__ . '/output.html.php';
 ?>
