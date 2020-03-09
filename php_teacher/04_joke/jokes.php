56d03922fddb9db697e156b3ea97c4c492660354
bum3144 <bum3144@gmail.com>
2020-03-09 10:37:30 +0800

    joke - jokes.php

--- php_teacher/04_joke/jokes.php
+++ php_teacher/04_joke/jokes.php
@@ -28,6 +28,8 @@ try{
     include __DIR__ . '/templates/jokes.html.php';
 
     $output = ob_get_clean(); // buffer return and remove buffer
+    // ob_start(); php가 실행되지만 브라우저로 전송되지않고 버퍼에 저장됨(내용을 출력하지 않음)
+    // $output = ob_get_clean(); include로 불러온 파일의 내용을 출력하지 않고 $output 변수에 담아둘 수 있다.
 
 }catch (PDOException $e){
 
