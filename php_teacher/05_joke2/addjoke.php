52cbf314708f847746ed32cb7f0531033506def7
bum3144 <bum3144@gmail.com>
2020-03-09 11:32:10 +0800

    joke2 - addjoke.php

---
+++ php_teacher/05_joke2/addjoke.php
@@ -0,0 +1,27 @@
+<?php
+/*
+1. If the joketext variable is not passed by POST, the input form is printed. 
+    (POST로 joketext 변수가 넘어오지 않으면 form을 출력한다.)
+2. When the joketext variable is passed, it adds a new joke text to the database.
+    (joketext 변수가 넘어오면 데이터베이스에 신규 유머 글을 추가한다.)
+*/
+if(isset($_POST['joketext'])){
+    try{
+        $pdo = new PDO('mysql:host=localhost;dbname=ijdb;charset=utf8;','ijdbuser','mypassword');
+        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);        
+    }catch (PDOException $e){
+        $title = "Database error!! <br> " . 
+        $e->getMessage() . "<br> Location file: " . $e->getFile() . "<br> Error line: " . $e->getLine();   
+    }
+}else{
+    $title = 'Add joke posts';
+
+    ob_start();
+
+    include __DIR__ . '/templates/addjoke.html.php';
+
+    $output = ob_get_clean();
+
+    include __DIR__ . '/templates/layout.html.php';
+}
+?>
