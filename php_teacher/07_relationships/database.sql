df9f2d7fa2cda7d140e4275eb44a8abe01b44061
bum3144 <bum3144@gmail.com>
2020-03-11 01:19:51 +0800

    Relationships - databases extention - Multi Tables Select (modify) - Many to One / Many to Many

--- php_teacher/07_relationships/database.sql
+++ php_teacher/07_relationships/database.sql
@@ -91,8 +91,64 @@ VALUES ('I dreamed I was forced to eat a giant marshmallow. When I woke up, my p
     -- FROM `joke` INNER JOIN `author`
     -- ON `authorid` = `author`.`id`';
 
+    -- many to one (joke - author) <==> one to many (author - joke)
     -- // Jeremy Park 이 작성한 글 전부 불러오기
     -- SELECT `joketext`, `name`, `email`
     -- FROM `joke` INNER JOIN `author`
     -- ON `authorid` = `author`.`id`
     -- WHERE `name` = "Jeremy Park";
+
+--------------------------------------------------------------------------
+    -- many to many 연습
+
+    CREATE TABLE `category` (
+        `id` INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+        `name` VARCHAR(255)
+    ) DEFAULT CHARACTER SET utf8 ENGINE=InnoDB;
+
+    -- ++++++ [ lookup 테이블 ] +++++ (joke <==> jokcategory <==> category)
+    CREATE TABLE `jokecategory` (
+        `jokeid` INT NOT NULL,
+        `categoryid` INT NOT NULL,
+        PRIMARY KEY (`jokeid`, `categoryid`)
+    ) DEFAULT CHARACTER SET utf8 ENGINE=InnoDB;
+
+    -- INSERT `category` SAMPLE DATA
+    INSERT INTO `category` (`id`, `name`)
+        VALUES  (1, 'Knock-knock'),
+                (2, 'Cross the road'),
+                (3, 'lawyers'),
+                (4, 'Walk the bar'),
+                (5, 'Light bulb');
+    
+    -- INSERT `jokecategory` SAMPLE DATA
+    INSERT INTO `jokecategory` (`jokeid`, `categoryid`)
+        VALUES  (1,2),
+                (2,1),
+                (2,4),
+                (3,3),
+                (1,5);
+
+    -- ex1) "knock-knock" => category `name` 가져오기
+    SELECT `joketext`,`jokeid`,`categoryid`,`name`
+    FROM `joke`
+    INNER JOIN `jokecategory`
+        ON `joke`.`id` = `jokeid`
+    INNER JOIN `category`
+        ON `categoryid` = `category`.`id`
+    WHERE `name` = 'knock-knock';
+
+    -- ex2) "I dreamed I was" joke `joketext` 가져오기
+    SELECT `joketext`,`jokeid`,`categoryid`,`name`
+    FROM `joke`
+    INNER JOIN `jokecategory`
+        ON `joke`.`id` = `jokeid`
+    INNER JOIN `category`
+        ON `categoryid` = `category`.`id`
+    WHERE `joketext` LIKE "I dreamed I was%";
+
+    -- DELETE `category` TABLE
+    DROP TABLE IF EXISTS `category`;
+    -- DELETE `jokecategory` TABLE
+    DROP TABLE IF EXISTS `jokecategory`;
+--------------------------------------------------------------------------
