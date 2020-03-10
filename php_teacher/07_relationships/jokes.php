065765b045b00c4932fd4f0a1a882c20678b0738
bum3144 <bum3144@gmail.com>
2020-03-11 00:15:49 +0800

    Relationships - Multi Tables Select (modify)

--- php_teacher/07_relationships/jokes.php
+++ php_teacher/07_relationships/jokes.php
@@ -1,28 +1,16 @@
 <?php
+// Query modify (select + join)
 try{
     $pdo = new PDO('mysql:host=localhost;dbname=ijdb;charset=utf8','ijdbuser','mypassword');
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-    $sql = 'SELECT `id`, `joketext` FROM `joke`';
-   
-    // 01. while
-    // $result = $pdo->query($sql);
-    // while($row = $result->fetch()){ 
-    //     $jokes[] = ['id' => $row['id'], 'joketext' => $row['joketext']]; 
-    // }  
+    // join할 두 쿼리(Multi Tables Select)
+    // SELECT `id`, LEFT(`joketext`, 20), `authorid` FROM `joke`
+    // SELECT * FROM `author`
+    $sql = 'SELECT `joke`.`id`, `joketext`, `name`, `email`
+    FROM `joke` INNER JOIN `author`
+    ON `authorid` = `author`.`id`';
 
-    // 02. foreach를 사용해 본다
-    // $result = $pdo->query($sql);
-    // foreach ($result as $row){ 
-    //     $jokes[] = array('id' => $row['id'], 'joketext' => $row['joketext']); 
-    // }
-
-    // 03. 템플릿에서 사용할 $jokes 변수는 배열이 아니라 PDOStatement 객체로 바뀌었다
-    // $result = $pdo->query($sql);
-    // $jokes = $result;
-
-
-    // 04. Finish
     $jokes = $pdo->query($sql);
 
     $title = 'Joke List';
