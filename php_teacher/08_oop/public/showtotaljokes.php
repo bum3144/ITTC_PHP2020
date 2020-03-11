5e32770a8fb2b92d82d284df62e97347c17f3aee
bum3144 <bum3144@gmail.com>
2020-03-11 17:26:41 +0800

    PHP code structuring techniques. Include Databases Connection file 구조적 프로그래밍으로 폴더 변경. 의존성 주입 기법 - 한 변수를 여러 곳에서 사용하는 기법

--- php_teacher/08_oop/public/showtotaljokes.php
+++ php_teacher/08_oop/public/showtotaljokes.php
@@ -1,4 +1,5 @@
 <?php
+// ## dependency injection 의존성 주입 기법이란 한 변수를 여러 곳에서 안전하게 사용하는 기법
 // An include file that creates the $pdo variable and connects to the database.
 // $pdo 변수를 생성하고 데이터베이스로 접속하는 인크루드 파일.
 include __DIR__ . '/../includes/DatabaseConnection.php';
