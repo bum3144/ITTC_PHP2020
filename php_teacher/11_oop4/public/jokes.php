7075ecec8fe30c51c8c803c67d97a66ddf663217
bum3144 <bum3144@gmail.com>
2020-03-13 23:30:02 +0800

    totalJokes() -> total() 변경, table명 추가

--- php_teacher/11_oop4/public/jokes.php
+++ php_teacher/11_oop4/public/jokes.php
@@ -3,17 +3,31 @@ try{
     include __DIR__ . '/../includes/DatabaseConnection.php';
     include __DIR__ . '/../includes/DatabaseFunctions.php';
 
-    $jokes = allJokes($pdo);
+    $result = findAll($pdo, 'joke');
+
+    $jokes = [];
+
+    foreach($result as $joke){
+        $author = findById($pdo, 'author', 'id', $joke['authorid']);
+
+        $jokes[] = [
+            'id' => $joke['id'],
+            'joketext' => $joke['joketext'],
+            'jokedate' => $joke['jokedate'],
+            'name' => $author['name'],
+            'email' => $author['email']
+        ];
+    }
+
 
     $title = 'Joke List';
 
-    $totalJokes = totalJokes($pdo);
+    // totalJokes() -> total() 변경, table명 추가
+    $totalJokes = total($pdo, 'joke');
      
     ob_start(); 
 
-    include __DIR__ . '/../templates/jokes.html.php'; // error 발생시 해당 파일을 비워두고 다음을 실행한다
-    // require __DIR__ . '/../templates/jokes.html.php'; // error 발생시 해당 지점에서 실행을 멈춘다
-    // php.ini 설정에서 경고와 오류 출력을 막는 경우가 많기 때문에 오류를 일으켜도 화면에 출력되지 않는 경우가 많다(아예 빈 페이지가 나올때도 있다)
+    include __DIR__ . '/../templates/jokes.html.php'; 
 
     $output = ob_get_clean(); 
 
