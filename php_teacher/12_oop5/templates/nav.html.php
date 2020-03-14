b49610df59c5119332ab4858aef7a9e7ba7351b6
bum3144 <bum3144@gmail.com>
2020-03-14 11:26:46 +0800

    addjoke, editjoke 파일 통합으로 링크 수정 editjoke.php

--- php_teacher/12_oop5/templates/nav.html.php
+++ php_teacher/12_oop5/templates/nav.html.php
@@ -5,6 +5,7 @@
     <ul>
         <li><a href="index.php">HOME</a></li>
         <li><a href="jokes.php">JOKE LIST</a></li>
-        <li><a href="addjoke.php">JOKE ADD</a></li>
+        <li><a href="editjoke.php">JOKE ADD</a></li>
+        <!-- addjoke.php 파일은 더이상 필요없다. editjoke에서 등록/수정 모두 처리함 -->
     </ul>
 </nav>        
