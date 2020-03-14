ce8cb43c42f5957a2cd31c5284015d0fac74543a
bum3144 <bum3144@gmail.com>
2020-03-14 11:28:15 +0800

    form 필드 데이터는 PHP 배열 형태로 전달

--- php_teacher/12_oop5/public/editjoke.php
+++ php_teacher/12_oop5/public/editjoke.php
@@ -3,19 +3,24 @@ include __DIR__ . "/../includes/DatabaseConnection.php";
 include __DIR__ . "/../includes/DatabaseFunctions.php";
 
 try{
-    if(isset($_POST['joketext'])){
+    if(isset($_POST['joke'])){
         // updateJoke() -> update(), table명 추가, primary key 추가
-        update($pdo, 'joke', 'id', [
-            'id' => $_POST['jokeid'], 
-            'joketext' => $_POST['joketext'], 
-            'authorid' => 1
-            ]);
+        // 이제는 update() 대신 save()함수로 대체한다           
+        // form 필드 데이터는 PHP 배열 형태로 전달된다 
+        // 전달된 $_POST['joke]는 단일값이 아니라 배열이다.
+        $joke = $_POST['joke'];
+        $joke['jokedate'] = new DateTime();
+        $joke['authorid'] = 1;
+
+        save($pdo, 'joke', 'id', $joke);
 
         header('location: jokes.php');
     }else{
-        $joke = getJoke($pdo, $_GET['id']);
-
-        $title = 'edit joke post';
+        if (isset($_GET['id'])){
+            $joke = findById($pdo, 'joke', 'id', $_GET['id']);
+        }
+        
+        $title = 'Edit joke post';
 
         ob_start();
 
@@ -28,7 +33,6 @@ try{
 
     $output = 'Database error : ' . $e->getMessage() . "<br> Location file: " . $e->getFile() . "<br> Error line: " . $e->getLine();   
 }
-
 
 include __DIR__ . "/../templates/layout.html.php";
 ?>
