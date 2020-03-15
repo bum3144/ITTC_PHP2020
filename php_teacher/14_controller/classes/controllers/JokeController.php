144aefe7395319a150c372f5a1956d4e7d16025c
bum3144 <bum3144@gmail.com>
2020-03-15 18:39:32 +0800

    front controll 프론트 컨트롤를 위한 수정 - template

---
+++ php_teacher/14_controller/classes/controllers/JokeController.php
@@ -0,0 +1,89 @@
+<?php
+
+class JokeController {
+    private $authorsTable;
+    private $jokesTable;
+
+	public function __construct(DatabaseTable $jokesTable, DatabaseTable $authorsTable) {
+        $this->jokesTable = $jokesTable;
+        $this->authorsTable = $authorsTable;
+    }
+
+    // jokes.php
+    public function list(){
+        $result = $this->jokesTable->findAll();
+
+        $jokes = [];
+        foreach($result as $joke){
+            $author = $this->authorsTable->findById($joke['authorid']);
+
+            $jokes[] = [
+                'id' => $joke['id'],
+                'joketext' => $joke['joketext'],
+                'jokedate' => $joke['jokedate'],
+                'name' => $author['name'],
+                'email' => $author['email']
+            ];
+        }
+
+        $title = 'Joke Post List';
+
+        $totalJokes = $this->jokesTable->total();
+
+        return ['template' => 'jokes.html.php', 
+                'title' => $title,
+                'variables' => [
+                        'totalJokes' => $totalJokes,
+                        'jokes' => $jokes
+                    ]
+                ];
+    }
+    // index.php
+    public function home(){
+        $title = 'Internet Joke World';
+
+        return ['template' => 'home.html.php', 'title' => $title];
+    }
+
+    // deletejoke.php
+    public function delete(){
+        $this->jokesTable->delete($_POST['id']);
+
+        header('Location: index.php?action=list');
+    }
+
+    // editjoke.php
+    public function edit(){
+        if(isset($_POST['joke'])){
+
+            $joke = $_POST['joke'];
+            $joke['jokedate'] = new DateTime();
+            $joke['authorid'] = 1;
+    
+            $this->jokesTable->save($joke);
+    
+            header('location: index.php?action=list');
+
+        }else{
+
+            if (isset($_GET['id'])){                
+                $joke = $this->jokesTable->findById($_GET['id']);
+            }
+            
+            $title = 'Edit joke post';
+                
+			return ['template' => 'editjoke.html.php',
+					'title' => $title,
+					'variables' => [
+							'joke' => $joke ?? null
+						]
+					];
+        }        
+    }
+
+
+}
+
+
+
+?>
