8c0f6c340d4c4e288836e29039ea164c2fef4712
bum3144 <bum3144@gmail.com>
2020-03-15 18:40:00 +0800

    front controll 프론트 컨트롤를 위한 링크 수정 - template

--- php_teacher/14_controller/templates/nav.html.php
+++ php_teacher/14_controller/templates/nav.html.php
@@ -4,8 +4,7 @@
     </header>
     <ul>
         <li><a href="index.php">HOME</a></li>
-        <li><a href="jokes.php">JOKE LIST</a></li>
-        <li><a href="editjoke.php">JOKE ADD</a></li>
-        <!-- addjoke.php 파일은 더이상 필요없다. editjoke에서 등록/수정 모두 처리함 -->
+        <li><a href="index.php?action=list">JOKE LIST</a></li>
+        <li><a href="index.php?action=edit">JOKE ADD</a></li>
     </ul>
 </nav>        
