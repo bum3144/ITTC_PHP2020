ffa965a298e3bf43b90ab32a851a6946c5109a2e
bum3144 <bum3144@gmail.com>
2020-03-16 00:20:48 +0800

    controller index.php

--- php_teacher/15_framework/public/index.php
+++ php_teacher/15_framework/public/index.php
@@ -12,21 +12,44 @@ function loadTemplate($templateFileName, $variables = []){
 try{
     include __DIR__ . '/../includes/DatabaseConnection.php';
     include __DIR__ . '/../classes/DatabaseTable.php';
-    include __DIR__ . '/../classes/controllers/JokeController.php';
 
     $jokesTable = new DatabaseTable($pdo, 'joke', 'id');
     $authorsTable = new DatabaseTable($pdo, 'author', 'id');
-    $jokeController = new JokeController($jokesTable, $authorsTable);
 
     $action = $_GET['action'] ?? 'home';
 
-    // 모든 URL을 소문자로 변경 
-    if($action == strtolower($action)){
-        $page = $jokeController->$action();
-    }else{
+    // route 변수가 없으면 'joke/home' 할당
+    $route = $_GET['route'] ?? 'joke/home';
+    // $route = ltrim(strtok($_SERVER['REQUEST_URL'], '?'), './');
+
+    if ($route == strtolower($route)){
+        if($route == 'joke/list'){
+            include __DIR__ . '/../classes/controllers/JokeController.php';
+            $controller = new JokeController($jokesTable, $authorsTable);
+            $page = $controller->list();
+        }elseif($route == 'joke/home'){
+            include __DIR__ . '/../classes/controllers/JokeController.php';
+            $controller = new JokeController($jokesTable, $authorsTable);
+            $page = $controller->home();
+        }elseif($route == 'joke/edit'){
+            include __DIR__ . '/../classes/controllers/JokeController.php';
+            $controller = new JokeController($jokesTable, $authorsTable);
+            $page = $controller->edit();
+        }elseif($route == 'joke/delete'){
+            include __DIR__ . '/../classes/controllers/JokeController.php';
+            $controller = new JokeController($jokesTable, $authorsTable);
+            $page = $controller->delete();
+        }elseif($route === 'register'){
+            include __DIR__ . '/../classes/controllers/RegisterController.php';
+            $controller = new RegisterController($authorsTable);
+            $page = $controller->showForm();
+        }
+
+    } else {
         http_response_code(301);
-        header('location: index.php?action=' . strtolower($action));
+        header('location: index.php?route=' . strtolower($route));
     }
+    
 
     $title = $page['title'];
 
