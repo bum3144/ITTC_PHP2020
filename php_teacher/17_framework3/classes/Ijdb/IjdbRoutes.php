91dc0f8ba7a4494dd364c38184c860e920ed957b
bum3144 <bum3144@gmail.com>
2020-03-16 23:52:54 +0800

    네임스페이스 사용 namespace Ijdb; new \Ittc\DatabaseTable new \Ijdb\Controllers\Joke

--- php_teacher/17_framework3/classes/Ijdb/IjdbRoutes.php
+++ php_teacher/17_framework3/classes/Ijdb/IjdbRoutes.php
@@ -1,32 +1,30 @@
 <?php
+namespace Ijdb;
+
 class IjdbRoutes 
 {
     public function callAction($route)
     {
         include __DIR__ . '/../../includes/DatabaseConnection.php';
-        
-        $jokesTable = new DatabaseTable($pdo, 'joke', 'id');
-        $authorsTable = new DatabaseTable($pdo, 'author', 'id');
+     
+        // namespace Ittc; namespace Ijdb; 네임스페이스 사용       
+        $jokesTable = new \Ittc\DatabaseTable($pdo, 'joke', 'id');
+        $authorsTable = new \Ittc\DatabaseTable($pdo, 'author', 'id');
 
         if($route === 'joke/list'){
-            include __DIR__ . '/../classes/controllers/JokeController.php';
-            $controller = new JokeController($jokesTable, $authorsTable);
+            $controller = new \Ijdb\Controllers\Joke($jokesTable, $authorsTable);
             $page = $controller->list();
         }elseif($route === ''){
-            include __DIR__ . '/../classes/controllers/JokeController.php';
-            $controller = new JokeController($jokesTable, $authorsTable);
+            $controller = new \Ijdb\Controllers\Joke($jokesTable, $authorsTable);
             $page = $controller->home();
         }elseif($route === 'joke/edit'){
-            include __DIR__ . '/../classes/controllers/JokeController.php';
-            $controller = new JokeController($jokesTable, $authorsTable);
+            $controller = new \Ijdb\Controllers\Joke($jokesTable, $authorsTable);
             $page = $controller->edit();
         }elseif($route === 'joke/delete'){
-            include __DIR__ . '/../classes/controllers/JokeController.php';
-            $controller = new JokeController($jokesTable, $authorsTable);
+            $controller = new \Ijdb\Controllers\Joke($jokesTable, $authorsTable);
             $page = $controller->delete();
         }elseif($route === 'register'){
-            include __DIR__ . '/../classes/controllers/RegisterController.php';
-            $controller = new RegisterController($authorsTable);
+            $controller = new \Ijdb\Controllers\Register($authorsTable);
             $page = $controller->showForm();
         }
 
