5f53dad79db1d9ef8694f6fb1f648770ca31c25f
bum3144 <bum3144@gmail.com>
2020-03-16 16:30:25 +0800

    EntryPoint class로 메서드 이동

--- php_teacher/16_framework2/public/index.php
+++ php_teacher/16_framework2/public/index.php
@@ -1,66 +1,11 @@
 <?php
-function loadTemplate($templateFileName, $variables = []){
-
-    extract($variables);
-
-    ob_start();    
-    include __DIR__ . '/../templates/' . $templateFileName;
-
-    return ob_get_clean(); 
-}
 try{
-    include __DIR__ . '/../includes/DatabaseConnection.php';
-    include __DIR__ . '/../classes/DatabaseTable.php';
+    include __DIR__ . '/../classes/EntryPoint.php';
     
-    $jokesTable = new DatabaseTable($pdo, 'joke', 'id');
-    $authorsTable = new DatabaseTable($pdo, 'author', 'id');
-    
-    $route = $_GET['route'] ?? 'joke/home';
-
-    // route 변수가 없으면 'joke/home' 할당
-    // $route = $_GET['route'] ?? 'joke/home';
-    
-    // $_SERVER['REQUEST_URI'] : 현재페이지의 주소에서 도메인 제거 ex) /joke/home?user=test
-    // strtok($_SERVER['REQUEST_URI'], '?') ? 와 이후 문자를 제거 ex) /joke/home
-    // ltrim(strtok($_SERVER['REQUEST_URI'], '?'), '/') 문자열 맨 앞에 있는 / 를 제거 ex) joke/home
     $route = ltrim(strtok($_SERVER['REQUEST_URI'], '?'), '/');
-
-     if ($route == strtolower($route)){
-        if($route === 'joke/list'){
-            include __DIR__ . '/../classes/controllers/JokeController.php';
-            $controller = new JokeController($jokesTable, $authorsTable);
-            $page = $controller->list();
-        }elseif($route === ''){
-            include __DIR__ . '/../classes/controllers/JokeController.php';
-            $controller = new JokeController($jokesTable, $authorsTable);
-            $page = $controller->home();
-        }elseif($route === 'joke/edit'){
-            include __DIR__ . '/../classes/controllers/JokeController.php';
-            $controller = new JokeController($jokesTable, $authorsTable);
-            $page = $controller->edit();
-        }elseif($route === 'joke/delete'){
-            include __DIR__ . '/../classes/controllers/JokeController.php';
-            $controller = new JokeController($jokesTable, $authorsTable);
-            $page = $controller->delete();
-        }elseif($route === 'register'){
-            include __DIR__ . '/../classes/controllers/RegisterController.php';
-            $controller = new RegisterController($authorsTable);
-            $page = $controller->showForm();
-        }
-
-    } else {
-        http_response_code(301);
-        header('location: index.php?route=' . strtolower($route));
-    }
     
-
-    $title = $page['title'];
-
-    if(isset($page['variables'])){
-        $output = loadTemplate($page['template'], $page['variables']);
-    }else{
-        $output = loadTemplate($page['template']);
-    }
+    $entryPoint = new EntryPoint($route);
+    $entryPoint->run();
 
 }catch (PDOException $e){
 
@@ -71,7 +16,6 @@ try{
     $e->getFile() . "<br> Error line: " . 
     $e->getLine();  
 
-}
-  
-include __DIR__ . '/../templates/layout.html.php';
+    include __DIR__ . '/../templates/layout.html.php';
+}  
 ?>
