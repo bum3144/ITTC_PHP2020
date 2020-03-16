947b28e5ef2cadeb3b4c8ca43d59cd87bdd78342
bum3144 <bum3144@gmail.com>
2020-03-16 23:48:17 +0800

    jokes.html.php

--- php_teacher/17_framework3/templates/jokes.html.php
+++ php_teacher/17_framework3/templates/jokes.html.php
@@ -1,31 +1,25 @@
 <p>There are [ <?=$variables['totalJokes'];?> ] joke posts.</p> 
 <?php foreach ($jokes as $jk): ?>
-    <blockquote> 
-        <p>
-        <?=htmlspecialchars($jk['joketext'], ENT_QUOTES, 'UTF-8');?>
+<blockquote> 
+    <p>
+    <?=htmlspecialchars($jk['joketext'], ENT_QUOTES, 'UTF-8');?>
 
-        (Author: <a href="mailto:<?php 
-            echo htmlspecialchars($jk['email'], ENT_QUOTES, 'UTF-8');?>"><?php 
-            echo htmlspecialchars($jk['name'], ENT_QUOTES, 'UTF-8');?></a>
-            Post date: 
-            <?php
-                $date = new DateTime($jk['jokedate']);
-                echo $date->format('jS F Y');
-                //echo $date->format('Y-m-d');
-            ?>)
+    (Author: <a href="mailto:<?=htmlspecialchars($jk['email'], ENT_QUOTES, 'UTF-8');?>">
+    <?=htmlspecialchars($jk['name'], ENT_QUOTES, 'UTF-8');?></a>
+        Post date: 
+        <?php
+            $date = new DateTime($jk['jokedate']);
+            
+            echo $date->format('jS F Y');
+        ?>)
 
-        <!-- <a href="index.php?action=edit&id=<?=$jk['id'];?>">EDIT</a>
 
-        <form action="index.php?action=delete" method="post">
-            <input type="hidden" name="id" value="<?=$jk['id'];?>">
-            <input type="submit" value="DEL">
-        </form> -->
-        <a href="/joke/edit?id=<?=$jk['id'];?>">EDIT</a>
+    <a href="/joke/edit?id=<?=$jk['id'];?>">EDIT</a>
 
-        <form action="/joke/delete" method="post">
-            <input type="hidden" name="id" value="<?=$jk['id'];?>">
-            <input type="submit" value="DEL">
-        </form>
-        </p>
-    </blockquote>
+    <form action="/joke/delete" method="post">
+        <input type="hidden" name="id" value="<?=$jk['id'];?>">
+        <input type="submit" value="DEL">
+    </form>
+    </p>
+</blockquote>
 <?php endforeach; ?>  
