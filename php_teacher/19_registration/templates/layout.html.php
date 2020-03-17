1fb0283916689e4abab7580d4458093869d2eb2c
bum3144 <bum3144@gmail.com>
2020-03-17 22:59:51 +0800

    <link rel="shortcut icon" href="#" />

--- php_teacher/19_registration/templates/layout.html.php
+++ php_teacher/19_registration/templates/layout.html.php
@@ -3,6 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="shortcut icon" href="#" />
     <link rel="stylesheet" href="/jokes.css"> 
     <title><?=$title?></title>
 </head>
