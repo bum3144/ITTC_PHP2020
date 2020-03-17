8075c578ac2a547634faadc57361cc030f5c466e
bum3144 <bum3144@gmail.com>
2020-03-17 22:46:43 +0800

    사용자 등록폼 작성 유효성 검사 후 에러 메시지 출력

--- php_teacher/19_registration/templates/register.html.php
+++ php_teacher/19_registration/templates/register.html.php
@@ -1,12 +1,31 @@
+<?php
+if(!empty($errors)):
+?>
+<div class="errors">
+    <p>You cannot register. Please check below.</p>
+    <ul>
+    <?php
+        foreach($errors as $error):
+    ?>
+        <li><?= $error; ?></li>        
+    <?php
+        endforeach;
+    ?>
+    </ul>
+</div>
+<?php
+endif;
+?>
+
 <form action="" method="post">
     <label for="email">Email</label>
-    <input name="author[email]" id="email" type="text">
+    <input name="author[email]" id="email" type="text" value="<?=$author['email'] ?? ''?>">
 
     <label for="name">Name</label>
-    <input name="author[name]" id="name" type="text">
+    <input name="author[name]" id="name" type="text" value="<?=$author['name'] ?? ''?>">
 
     <label for="password">Password</label>
-    <input name="author[password]" id="password" type="text">
+    <input name="author[password]" id="password" type="text" value="<?=$author['password'] ?? ''?>">
 
     <input type="submit" name="submit" value="Add User">
 </form>
