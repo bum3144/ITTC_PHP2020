02983eeed08bd4c1f04de85e233d6a26d31800e8
bum3144 <bum3144@gmail.com>
2020-03-17 15:25:49 +0800

    edit() -> 폼 표시 메서드와 폼 처리 메서드로 코드 분리한다 폼 표시 메서드 saveEdit(), 처리 메서드 edit()

--- php_teacher/18_composer/classes/Ijdb/Controllers/Joke.php
+++ php_teacher/18_composer/classes/Ijdb/Controllers/Joke.php
@@ -11,7 +11,6 @@ class Joke {
         $this->authorsTable = $authorsTable;
     }
 
-    // jokes.php
     public function list(){
         $result = $this->jokesTable->findAll();
 
@@ -40,50 +39,50 @@ class Joke {
                     ]
                 ];
     }
-    // index.php
+
     public function home(){
         $title = 'Internet Joke World';
 
         return ['template' => 'home.html.php', 'title' => $title];
     }
 
-    // deletejoke.php
     public function delete(){
         $this->jokesTable->delete($_POST['id']);
 
-        // header('Location: index.php?action=list');
         header('Location: /joke/list');
     }
 
-    // editjoke.php
-    public function edit(){
-        if(isset($_POST['joke'])){
+    // 폼 표시 메서드와 폼 처리 메서드로 코드 분리한다
+    // 폼 표시 메서드 saveEdit(), 처리 메서드 edit()
+    public function saveEdit(){
 
-            $joke = $_POST['joke'];
-            $joke['jokedate'] = new \DateTime();
-            $joke['authorid'] = 1;
-    
-            $this->jokesTable->save($joke);
-    
-            // header('location: index.php?action=list');
-            header('location: /joke/list');
+        $joke = $_POST['joke'];
+        $joke['jokedate'] = new \DateTime();
+        $joke['authorid'] = 1;
 
-        }else{
+        $this->jokesTable->save($joke);
 
-            if (isset($_GET['id'])){                
-                $joke = $this->jokesTable->findById($_GET['id']);
-            }
-            
-            $title = 'Edit joke post';
-                
-			return ['template' => 'editjoke.html.php',
-					'title' => $title,
-					'variables' => [
-							'joke' => $joke ?? null
-						]
-					];
-        }        
+        header('location: /joke/list');
+
     }
+
+    public function edit()
+    {
+        if (isset($_GET['id'])){                
+            $joke = $this->jokesTable->findById($_GET['id']);
+        }
+        
+        $title = 'Edit joke post';
+            
+        return ['template' => 'editjoke.html.php',
+            'title' => $title,
+            'variables' => [
+                'joke' => $joke ?? null
+            ]
+        ];
+    }
+        
+    
 
 
 }
