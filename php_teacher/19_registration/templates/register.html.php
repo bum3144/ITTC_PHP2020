76adf4aa60f1411eb99b39d4b86168fcd454a9fe
bum3144 <bum3144@gmail.com>
2020-03-17 21:16:19 +0800

    register form templates 생성

---
+++ php_teacher/19_registration/templates/register.html.php
@@ -0,0 +1,12 @@
+<form action="" method="post">
+    <label for="email">Email</label>
+    <input name="author[email]" id="email" type="text">
+
+    <label for="name">Name</label>
+    <input name="author[name]" id="name" type="text">
+
+    <label for="password">Password</label>
+    <input name="author[password]" id="password" type="text">
+
+    <input type="submit" name="submit" value="Add User">
+</form>
