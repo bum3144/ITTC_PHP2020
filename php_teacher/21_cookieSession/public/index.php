5d3fcb948fafad218b063b87c46a8b98aaf2ffa0
bum3144 <bum3144@gmail.com>
2020-03-18 23:46:21 +0800

    $_COOKIE - setcookie

--- php_teacher/21_cookieSession/public/index.php
+++ php_teacher/21_cookieSession/public/index.php
@@ -1,5 +1,20 @@
 <?php
 //   echo phpinfo(); die;
+
+if(!isset($_COOKIE['visits'])){
+    $_COOKIE['visits'] = 0;
+}
+$visits = $_COOKIE['visits'] + 1;
+setcookie('visits', $visits, time() + 3600 * 24 * 365);
+
+if($visits > 1){
+    echo "$visits 번째 방문하셨습니다.";
+}else{
+    // 첫 방문
+    echo "웹사이트에 오신 걸 환영합니다. 둘러보려면 여기를 클릭하세요!";
+}
+
+
 try{
     include __DIR__ . '/../includes/autoload.php';
     
