c8804e63485841f8f9277acaf96b01d2b0296658
bum3144 <bum3144@gmail.com>
2020-03-19 23:54:37 +0800

    Login, Louout 메뉴 추가

--- php_teacher/22_logout/templates/nav.html.php
+++ php_teacher/22_logout/templates/nav.html.php
@@ -3,11 +3,14 @@
         <h1>INTERNET JOKE WORLD</h1>
     </header>
     <ul>
-        <!-- <li><a href="index.php">HOME</a></li>
-        <li><a href="index.php?route=joke/list">JOKE LIST</a></li>
-        <li><a href="index.php?route=joke/edit">JOKE ADD</a></li> -->
         <li><a href="/">HOME</a></li>
         <li><a href="/joke/list">JOKE LIST</a></li>
         <li><a href="/joke/edit">JOKE ADD</a></li>
+
+        <?php if ($loggedIn): ?>
+        <li><a href="/logout">Logout</a></li>
+        <?php else: ?>
+        <li><a href="/login">Log In</a></li>
+        <?php endif; ?>
     </ul>
 </nav>        
